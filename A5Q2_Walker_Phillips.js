089c46d2f500f6006a74e75ab48415f9b5888405
walkerphillips04 <124199383+walkerphillips04@users.noreply.github.com>
2023-04-16 21:46:04 -0400

    Create A5Q2_Walker_Phillips.js

---
+++ A5Q2_Walker_Phillips.js
@@ -0,0 +1,71 @@
+let meteors = [];
+
+class meteor {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.xSpeed = random(-1, 1);
+    this.ySpeed = random(-1, 1);
+  }
+
+  move() {
+      this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    
+
+    if (this.x + this.size / 2 > width) {
+      this.x = width - this.size / 2;
+      this.xSpeed *= -1; 
+    } else if (this.x - this.size / 2 < 0) {
+      this.x = this.size / 2;
+      this.xSpeed *= -1; 
+    }
+    
+    if (this.y + this.size / 2 > height) {
+      this.y = height - this.size / 2;
+      this.ySpeed *= -1; 
+    } else if (this.y - this.size / 2 < 0) {
+      this.y = this.size / 2;
+      this.ySpeed *= -1; 
+    }
+  }
+
+  update() {
+    this.move();
+    this.checkEdges();
+  }
+
+  show() {
+    noStroke();
+    fill("gray");
+    rect(this.x, this.y, this.size);
+  }
+
+  checkEdges() {
+    if (this.x > width || this.x < 1) {
+      this.x = width - this.x;
+    }
+    if (this.y > height || this.y < 1) {
+      this.y = height - this.y;
+    }
+  }
+}
+
+function setup() {
+  createCanvas(960, 540);
+  for (let i = 0; i < 60; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(20, 50);
+    meteors.push(new meteor(x, y, size));
+  }
+}
+
+function draw() {
+  background(10);
+  for (let meteor of meteors) {
+    meteor.update();
+    meteor.show();
+  }
+}
